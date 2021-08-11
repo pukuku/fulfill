8b3c18adf34cd2410db669e2e4e3bd443f3f1671
pukuku <rktar.yjp@icloud.com>
2021-08-11 05:35:59 +0000

    アプリケーションの基礎画面 #6

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,4 +1,11 @@
 class HelpsController < ApplicationController
-  def index
+
+def index
+  if params[:search_word] == nil
+    @helps = Help.all
+  else
+    @helps = Help.search(params[:search_word])
   end
+end
+
 end
